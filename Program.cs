f46c64705b17d65aac9b8439bc035eb01f1035ae
Michael Meienberg <mei@acs-ag.ch>
2020-04-12 17:49:57 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace helloworlddocker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
