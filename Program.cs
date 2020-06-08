9a4dcb34fce57918c1b06b2183f66c0babb04192
Michael Meienberg <mei@acs-ag.ch>
2020-06-08 11:51:01 +0200

    k3s test

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace helloworlddocker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World, Windows !!!");
+            Console.WriteLine("Hello World, Windows (from k3s) !!!");
         }
     }
 }
