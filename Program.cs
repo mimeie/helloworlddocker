b4a3b049987836ea810fb7147af45d3c9f071ac7
Michael Meienberg <mei@acs-ag.ch>
2020-06-08 12:01:33 +0200

    next

--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@ namespace helloworlddocker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World, Windows (from k3s) !!!");
+            Console.WriteLine("Hello World, Windows (1 from k3s) !!!");
+
+
+            Console.WriteLine("Hello World, Windows (2 from k3s) !!!");
+
+            Console.WriteLine("Hello World, Windows (3 from k3s) !!!");
         }
     }
 }
